46ba70c6059cb3391d8aa6694147209e7d85add3
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-06-08 19:01:51 -0300

    feat: task container

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,61 @@
-import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { PiShareDuotone, PiTrash } from "react-icons/pi";
 
 import { getSession } from 'next-auth/react';
+
+import styles from './styles.module.css';
+import { Textarea } from "@/components/Textarea";
 
 export default function Dashboard() {
     return (
         <>
             <Head>
-                <title>Dashboard</title>
+                <title>Meu painel de tarefas</title>
             </Head>
-            <h1>Dashboard</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual a tarefa?</h1>
+                        <form>
+                            <Textarea
+                                placeholder="Digite sua tarefa"
+                            />
+                            <div className={styles.checkboxArea}>
+                                <input
+                                    type="checkbox"
+                                    className={styles.checkbox}
+                                />
+                                <label className={styles.label}>Deixar tarefa pública?</label>
+                            </div>
+                            <button type="submit" className={styles.button}>Registrar</button>
+                        </form>
+                    </div>
+                </section>
+                <section className={styles.taskContainer}>
+                    <h1 className={styles.titleTask}>Minhas tarefas</h1>
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PÚBLICO</label>
+                            <button className={styles.tagButton}>
+                                <PiShareDuotone
+                                    size={22}
+                                    color="#3183ff"
+                                />
+                            </button>
+                        </div>
+                        <div className={styles.taskContent}>
+                            <p>Tarefa 1</p>
+                            <button className={styles.buttonTrash}>
+                                <PiTrash
+                                    size={24}
+                                    color="#ea3140"
+                                />
+                            </button>
+                        </div>
+                    </article>
+                </section>
+            </main>
         </>
     )
 }
@@ -26,7 +72,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
             },
         };
     };
-    
+
     return {
         props: {}
     };
